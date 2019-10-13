736e0fef388548e9635f34d6c295a834bdbfbcb5
lily <921180142@qq.com>
2019-10-13 11:25:27 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  $('.head').load("header.html", "", function (response, status, request) {
+    $(this).html(response);
+  });
+
+  $('.footers').load("footer.html", "", function (response, status, request) {
+    $(this).html(response);
+  });
+
+  
+
     $("body").delegate(".hamburger", "click", function () {
       $(this).toggleClass("is-active");
       $('.nav-mobile').toggleClass('active')
