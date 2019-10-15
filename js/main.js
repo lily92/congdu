cfa535604d891a59639f359200c1ad4c9579c856
lily <921180142@qq.com>
2019-10-15 16:59:36 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
- $(function(){
+$(function () {
   $('#_header').load("header.html", "", function (response, status, request) {
     $('#_header').html(response);
   });
@@ -6,7 +6,7 @@
   $('.footers').load("footer.html", "", function (response, status, request) {
     $(this).html(response);
   });
-  
+
   $(document).ready(function () {
     $("#_header").on("click", ".hamburger", function () {
       $(this).toggleClass("is-active");
@@ -25,7 +25,6 @@
       }
     });
 
-
     $('.show-menu').click(function () {
       if ($(this).text() == '+') {
         $(this).parent().parent().parent().find('.show-menu').text('+');
@@ -37,6 +36,14 @@
         $(this).parent().parent().find('dd').stop().slideUp();
       }
     })
+
+    /** pc hover */
+    $("#_header").on("mouseenter", ".nav-right li", function () {
+      console.log($('nav').width())
+      var nav_w = $('nav').width()
+      $('.nav-line').attr('class','nav-line line'+$(this).index())
+    });
+
   })
- })
+})
 
